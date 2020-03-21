93c8d2d09904cf04101feb79453cda53869dd70d
venegas <venegas_@hotmail.com>
2020-03-21 17:35:38 -0500

    Se aplico un espacio.

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -3,6 +3,7 @@ import 'package:flutter_vscode/src/pages/contador_page.dart';
 //import 'package:flutter_vscode/src/pages/home_page.dart';
 class MyApp extends StatelessWidget{
   @override
+
   Widget build( context ){
     return MaterialApp(
       debugShowCheckedModeBanner: false,
